e24fcdffa851ba62b657dc0ced053b7e28544111
richiethom@gmail.com <richiethom@gmail.com@0f731ee1-ccb8-51f1-07f3-7c927eddca4b>
2010-06-04 10:17:34 +0000

    Making progress, much more modular now

--- trunk/src/play/modules/protocolbuffers/PlayBinaryHandler.java
+++ trunk/src/play/modules/protocolbuffers/PlayBinaryHandler.java
@@ -1,6 +1,8 @@
 package play.modules.protocolbuffers;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.SocketTimeoutException;
@@ -13,7 +15,6 @@ import org.quickserver.net.server.DataMode;
 import org.quickserver.net.server.DataType;
 
 import play.Play;
-import playmessages.Playmessages.PlayRequestWrapper;
 
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.Descriptors.FieldDescriptor;
@@ -24,23 +25,49 @@ public class PlayBinaryHandler implements ClientBinaryHandler {
 	public void handleBinary(ClientHandler clientHandler, byte[] bytesFromClient)
 			throws SocketTimeoutException, IOException {
 		System.out.println("Processing");
-		final PlayRequestWrapper wrapper = PlayRequestWrapper.parseFrom(bytesFromClient);
-		final Map<FieldDescriptor, Object> allFields = wrapper.getAllFields();
-		for (Entry<FieldDescriptor, Object> entry : allFields.entrySet()) {
-			final Object messageToHandle = entry.getValue();
-			//find a suitable method in the controller
-			try {
-				final Method methodToInvoke = findMethodForMessage(messageToHandle);
-				final GeneratedMessage result = (GeneratedMessage)methodToInvoke.invoke(null, messageToHandle);
-				System.out.println("ClientHandler is:"+clientHandler.getClass().getName());
-				clientHandler.setDataMode(DataMode.BINARY, DataType.OUT);
-				clientHandler.sendClientBinary(result.toByteArray());
-				clientHandler.closeConnection();
-				System.out.println("Response sent");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		try {
+			final String incomingRequestWrapperName = (String)Play.configuration.get("gpb.incoming.request.wrapper");
+			final Class<?> incomingRequestWrapper = Class.forName(incomingRequestWrapperName);
+			final Method parseFromMethod = incomingRequestWrapper.getMethod("parseFrom", Array.newInstance(byte.class, 0).getClass());
+			final GeneratedMessage wrapper = (GeneratedMessage)parseFromMethod.invoke(null, bytesFromClient);
+			final Map<FieldDescriptor, Object> allFields = wrapper.getAllFields();
+			for (Entry<FieldDescriptor, Object> entry : allFields.entrySet()) {
+				final Object messageToHandle = entry.getValue();
+				//find a suitable method in the controller
+				try {
+					final Method methodToInvoke = findMethodForMessage(messageToHandle);
+					final GeneratedMessage result = (GeneratedMessage)methodToInvoke.invoke(null, messageToHandle);
+					System.out.println("ClientHandler is:"+clientHandler.getClass().getName());
+					clientHandler.setDataMode(DataMode.BINARY, DataType.OUT);
+					clientHandler.sendClientBinary(result.toByteArray());
+					clientHandler.closeConnection();
+					System.out.println("Response sent");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeArraySizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 
@@ -49,7 +76,7 @@ public class PlayBinaryHandler implements ClientBinaryHandler {
 	private Method findMethodForMessage(Object messageToHandle) throws Exception {
 		System.out.println("Message:"+messageToHandle.getClass().getName());
 		final String controller = (String) Play.configuration.get("gpb.controller");
-		final Method[] methods = Class.forName(controller).getMethods();
+		final Method[] methods = Class.forName(controller,false, Play.classloader).getMethods();
 		for (Method method : methods) {
 			final Class<?>[] parameterTypes = method.getParameterTypes();
 			if (parameterTypes.length == 1 && parameterTypes[0].equals(messageToHandle.getClass()) && Modifier.isStatic(method.getModifiers())) {
